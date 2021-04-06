7be105665d9faa39ec936f84205b85cc42db7d02
9503chl <9503chl@naver.com>
2021-04-06 23:46:05 +0900

    '21.04.06'

--- VRBasketball/Assets/Scripts/ball.cs
+++ VRBasketball/Assets/Scripts/ball.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class ball : MonoBehaviour
 {
-    [SerializeField] float speed = 10f;
+    [SerializeField] float speed = 5f;
     void Start()
     {
         var velocity = speed * transform.forward;
         var rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.SendMessage("Scored");
     }
 }
