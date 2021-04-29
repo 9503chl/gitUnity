ab7ef2658df76c5f7e3b237de09996a2335eedcb
9503chl <9503chl@naver.com>
2021-04-29 16:18:03 +0900

    '21.04.29'

--- VRBasketball/Assets/Scripts/Shooter.cs
+++ VRBasketball/Assets/Scripts/Shooter.cs
@@ -6,6 +6,15 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefabs;
     [SerializeField] Transform hands;
+    [SerializeField] float bulletInterval = 0.5f;
+
+    void OnEnable(){
+        InvokeRepeating("Shoot", 1.0f, bulletInterval); // Invoke 설정시간마다 호출함.
+    }
+    void OnDisable()
+    {
+        CancelInvoke("Shoot"); //호출 취소
+    }
 
     void Update()
     {
